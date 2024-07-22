72392cdbc1cd01615cc8cd63843b49b0cd786810
VEDANT MOHOL <152087070+vedantmohol@users.noreply.github.com>
2024-07-22 13:03:26 +0530

    Update Server.cjs

--- Server.cjs
+++ Server.cjs
@@ -83,7 +83,7 @@ app.post('/api/send-email', async (req, res) => {
   }
 });
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8888;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
